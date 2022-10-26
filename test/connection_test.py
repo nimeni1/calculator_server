b4526b54795de2245d462a628bc31553db027dba
nimeni nimeni <dobremaximilian@yahoo.com>
2022-10-26 19:15:59 +0200

    add fixtures to connection_test
    add fixtures to improve readability and reduce redundant code

--- test/connection_test.py
+++ test/connection_test.py
@@ -1,18 +1,23 @@
 from fastapi.testclient import TestClient
+import pytest
+
 
 from main import app
 
 
-def test_read_main():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    test_client = TestClient(app)
+    return test_client
+
+
+def test_read_main(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
 
-def test_websocket():
-    client = TestClient(app)
+def test_websocket(client):
     with client.websocket_connect("/ws") as websocket:
         data = websocket.receive_json()
         assert data == {"msg": "Hello WebSocket"}
-
